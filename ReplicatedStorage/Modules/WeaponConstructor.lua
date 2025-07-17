7f4d9b8c82f3474fd24e60f3e685a57eaf1403e3
Inithenry69 <henryspotifyacc50@gmail.com>
2025-07-17 19:20:36 +0100

    Updated ReplicatedStorage/Modules/WeaponConstructor.lua

---
+++ ReplicatedStorage/Modules/WeaponConstructor.lua
@@ -0,0 +1,31 @@
+-- @ScriptType: ModuleScript
+
+
+
+export type WeaponType = {
+	name : string;
+	firetype : string; -- auto / semi
+	firerate : number;	
+}
+
+
+local Weapon = {}
+Weapon.__index = Weapon
+
+function Weapon.New(WeaponProperties : WeaponType)
+	local self = setmetatable({}, Weapon)
+	
+	self.WeaponProperties = WeaponProperties
+	
+	return self	
+end
+
+-- update
+
+	-- viewmodel
+
+	-- fire
+
+-- reload
+
+return Weapon
