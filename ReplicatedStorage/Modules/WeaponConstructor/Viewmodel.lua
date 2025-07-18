e1a2f1ee0e8576b4c31d97b2846a6000f2273292
Inithenry69 <henryspotifyacc50@gmail.com>
2025-07-18 18:35:57 +0100

    Updated ReplicatedStorage/Modules/WeaponConstructor/Viewmodel.lua

--- ReplicatedStorage/Modules/WeaponConstructor/Viewmodel.lua
+++ ReplicatedStorage/Modules/WeaponConstructor/Viewmodel.lua
@@ -1,4 +1,18 @@
 -- @ScriptType: ModuleScript
-local module = {}
 
-return module
+
+
+local Viewmodel = {}
+Viewmodel.__index = Viewmodel
+
+local ViewmodelModel = game:GetService("ReplicatedStorage").Assets.Viewmodel
+
+function Viewmodel.New()
+	
+	local self = setmetatable({}, Viewmodel)
+		
+	return self
+	
+end
+
+return Viewmodel
