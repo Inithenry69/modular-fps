e05c0f00b2ad0e55ef82f53b9f172f3ce7db7c14
Inithenry69 <henryspotifyacc50@gmail.com>
2025-07-19 10:26:31 +0100

    Updated StarterPlayer/StarterCharacterScripts/Client/WeaponHandler.lua

--- StarterPlayer/StarterCharacterScripts/Client/WeaponHandler.lua
+++ StarterPlayer/StarterCharacterScripts/Client/WeaponHandler.lua
@@ -3,3 +3,11 @@ local WeaponConstructor = require(game.ReplicatedStorage.Modules.WeaponConstruct
 
 local Honeybadger = WeaponConstructor.New("Honeybadger", game.ReplicatedStorage.Assets.Weapons.Honeybadger)
 Honeybadger:Setup()
+
+Honeybadger:Equip()
+
+game:GetService("RunService"):BindToRenderStep("Viewmodel", Enum.RenderPriority.Camera.Value + 1, function()
+	
+	Honeybadger:Update()
+	
+end)
