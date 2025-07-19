93e1484343c2b4eab6d5b97d65ba055b8fa0b01a
Inithenry69 <henryspotifyacc50@gmail.com>
2025-07-19 09:31:12 +0100

    Updated StarterPlayer/StarterCharacterScripts/Client/WeaponHandler.lua

--- StarterPlayer/StarterCharacterScripts/Client/WeaponHandler.lua
+++ StarterPlayer/StarterCharacterScripts/Client/WeaponHandler.lua
@@ -1,1 +1,5 @@
 -- @ScriptType: LocalScript
+local WeaponConstructor = require(game.ReplicatedStorage.Modules.WeaponConstructor)
+
+local Honeybadger = WeaponConstructor.New("Honeybadger", game.ReplicatedStorage.Assets.Weapons.Honeybadger)
+Honeybadger:Setup()
