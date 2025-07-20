1f5ecdff9435165e9f1bc9564a8ef8740d52fce3
Inithenry69 <henryspotifyacc50@gmail.com>
2025-07-20 17:43:29 +0100

    Updated ReplicatedStorage/Modules/WeaponConstructor/Viewmodel.lua

--- ReplicatedStorage/Modules/WeaponConstructor/Viewmodel.lua
+++ ReplicatedStorage/Modules/WeaponConstructor/Viewmodel.lua
@@ -19,10 +19,14 @@ function Viewmodel.New(WeaponModel)
 	
 	self.WeaponModel = WeaponModel
 	self.AnimationFolder = WeaponModel.Settings
+	self.WeaponProperties = require(WeaponModel.Settings)
 	
 	-- springs
 	self.SwayingSpring = SpringModule.new()
 	self.RecoilSpring = SpringModule.new()
+	self.KickSpring = SpringModule.new()
+	
+	self.shots = 0
 	
 	return self
 	
@@ -49,12 +53,53 @@ function Viewmodel:Update(dt)
 	local MouseDelta = UserInputService:GetMouseDelta()
 	
 	self.SwayingSpring:shove(Vector3.new(-MouseDelta.X / 500, MouseDelta.Y / 200, 0))
+
 	
 	local UpdatedSwayingSpring = self.SwayingSpring:update(dt)
+	local UpdatedRecoilSpring = self.RecoilSpring:update(dt)
+	local UpdatedKickSpring = self.KickSpring:update(dt)
 	
-	ViewmodelModel.PrimaryPart.CFrame = Camera.CFrame *
-		CFrame.new(UpdatedSwayingSpring.X, UpdatedSwayingSpring.Y, 0)
+	-- update viewmodel
+	ViewmodelModel.PrimaryPart.CFrame = Camera.CFrame 
+	
+	--kick
+	ViewmodelModel.PrimaryPart.CFrame *= CFrame.new(UpdatedSwayingSpring.X, UpdatedSwayingSpring.Y, 0)
+	ViewmodelModel.PrimaryPart.CFrame *= CFrame.new(0, 0, UpdatedKickSpring.Z)	
+	
+	-- update camera	
+	Camera.CFrame *= CFrame.Angles(math.rad(UpdatedRecoilSpring.X), math.rad(UpdatedRecoilSpring.Z), 0)
+end
+
+function Viewmodel:Recoil(isSpray)
+	
+	local RECOILS = self.WeaponProperties.RECOILS
+	
+	if isSpray then
 		
+		self.shots += 1
+		
+		if self.shots >= #RECOILS then
+			
+			self.shots = 1
+			
+		end
+		
+		self.RecoilSpring:shove(
+			Vector3.new(RECOILS[self.shots][1], 0, RECOILS[self.shots][2])
+		)
+		
+		self.KickSpring:shove(
+			self.WeaponProperties.KICK
+		)
+		
+	end
+	
+	if isSpray == false then
+		
+		self.shots = 0
+		
+	end
+	
 end
 
 return Viewmodel
