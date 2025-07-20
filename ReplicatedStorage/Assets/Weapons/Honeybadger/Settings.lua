a833349b04268575bf15ff0d1d77fcf7045fd9d9
Inithenry69 <henryspotifyacc50@gmail.com>
2025-07-20 17:43:29 +0100

    Updated ReplicatedStorage/Assets/Weapons/Honeybadger/Settings.lua

--- ReplicatedStorage/Assets/Weapons/Honeybadger/Settings.lua
+++ ReplicatedStorage/Assets/Weapons/Honeybadger/Settings.lua
@@ -1,4 +1,27 @@
 -- @ScriptType: ModuleScript
-local module = {}
+local Settings = {
+	
+	--[[
+		TODO Guide for making recoils
+	]]--
+	
+	RECOILS = {
+		{1, 0}, -- vertical, horizontal
+		{1, 0},
+		{1, 0},
+		{1, 0.05},
+		{1, -0.3},
+		{1, -0.3},
+		{1.2, -0.3},
+		{1.2, 0.3},
+		{1.2, 0.3},
+		{1, 0.3},
+		{1, 0.3},
+		{1, -0.3},
 
-return module
+	},
+	KICK = Vector3.new(0, 0, 1)
+	
+}
+
+return Settings
